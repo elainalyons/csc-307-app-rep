2609ce4e7a5173a2748785536d2c634441438519
Elaina Lyons <elainal88@icloud.com>
2026-01-18 13:50:14 -0800

    /users/:id GET endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,6 +39,9 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
@@ -47,6 +50,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
